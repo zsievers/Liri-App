46927219641155a477c7e9e569abb0620831c3f8
Zach <zjsievers@gmail.com>
2020-04-10 14:20:20 -0400

    starting on movie-this command

--- liri.js
+++ liri.js
@@ -1,25 +1,24 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-
 var Spotify = require('node-spotify-api');
-
-// var bandsintown = require('bandsintown')(codingbootcamp);
-
-// var omdb = require('omdb');
+var axios = require("axios");
+var omdb = require('omdb');
+var bandsintown = require('bandsintown')(faebdcb4e9367a333fbae3f0451c4891);
 
 var spotify = new Spotify(keys.spotify);
-// var bandsintown = require('bandsintown')(faebdcb4e9367a333fbae3f0451c4891);
-// var bandsintown = require('bandsintown')(codingbootcamp);
-// var omdb = require('omdb');
 
+
+// var bandsintown = require('bandsintown')(codingbootcamp);
+
+// ==========================================================================================================
+// spotify
 if (process.argv[2] === "spotify-this-song"){
     var song = process.argv.slice(3, process.argv.length).join(" ");
 } 
 else {
     var song = process.argv.slice(2, process.argv.length).join(" ");
-}
-
+};
 
 spotify.search({ type: 'track', query: song, limit: "3"}, function(err, data) {
         if (err) {
@@ -38,5 +37,16 @@ spotify.search({ type: 'track', query: song, limit: "3"}, function(err, data) {
             }
       });
 
+// =================================================================================================================
+// OMDB
+
+if (process.argv[2] === "movie-this"){
+    var movie = process.argv.slice(3, process.argv.length).join(" "); 
+}
+else {
+    var movie = process.argv.slice(2, process.argv.length).join(" ");
+}
+     
+axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy")      
 
 
