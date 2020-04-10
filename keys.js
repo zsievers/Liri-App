ba69c438d87e965f09c44ad7ca18099db2684403
Zach <zjsievers@gmail.com>
2020-04-10 10:35:54 -0400

    setting up spotify api

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log("loaded");
+
+exports.spotifyKeys = {
+    key: "99971584172e44e49019d48e365ed825",
+    secret: "d8473711650b4e579e5a8c176f36e2a7",
+}
+
+exports.omdbKeys = {
+    key: "b2365a1a"
+}
--- liri.js
+++ liri.js
@@ -1,1 +1,16 @@
-console.log("hello");
+var axios = require("axios");
+
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+    id: "9971584172e44e49019d48e365ed825",
+    secret: "d8473711650b4e579e5a8c176f36e2a7"
+  });
+
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+  });
