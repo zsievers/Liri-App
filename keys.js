06fbf2c674fb7f795cf792c7b2ca2cd73c688977
Zach <zjsievers@gmail.com>
2020-04-10 11:53:17 -0400

    .env // random.txt // keys.js updated

--- keys.js
+++ keys.js
@@ -1,10 +1,6 @@
-console.log("loaded");
+console.log("this is loaded");
 
 exports.spotify = {
-    key: "99971584172e44e49019d48e365ed825",
-    secret: "d8473711650b4e579e5a8c176f36e2a7",
-}
-
-exports.omdb = {
-    key: "b2365a1a"
-}
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+};
--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+
 var Spotify = require('node-spotify-api');
 
 // var spotify = new Spotify ({
@@ -9,3 +10,18 @@ var Spotify = require('node-spotify-api');
 // });
  
 var spotify = new Spotify(keys.spotify);
+// var bandsintown = require('bandsintown')(faebdcb4e9367a333fbae3f0451c4891);
+// // var bandsintown = require('bandsintown')(codingbootcamp);
+
+// var omdb = require('omdb');
+
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+  });
+
+
+
